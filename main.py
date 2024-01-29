dbdd4d129c1b6df183d02189d59a1096f0b492d7
Fabrizio <fabrizionapoli50@gmail.com>
2024-01-29 07:46:27 -0300

    primeira integracao com o banco feita (get e post)

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 from fastapi import FastAPI
+from sqlmodel import SQLModel, create_engine, Session, select
+from models.Game import Game
 
 app = FastAPI()
 
+engine = create_engine("sqlite:///database.sqlite")
+
+SQLModel.metadata.create_all(engine)
+
 @app.get('/')
 def start():
-    print('Welcome to my API')
+    return 'Welcome to my API'
+
+@app.get('/show_products')
+def get_products():
+    with Session(engine) as session:
+        statement = select(Game)
+        result = session.exec(statement).all()
+    return result
+
+@app.post('/insert_product')
+def post_product(product: Game):
+    with Session(engine) as session:
+        session.add(product)
+        session.commit()
+    return 'The product has been inserted'
--- models/Game.py
+++ models/Game.py
@@ -1,7 +1,9 @@
-from pydantic import BaseModel
 from decimal import Decimal
+from sqlmodel import SQLModel, Field
+from typing import Optional
 
-class Game(BaseModel):
+class Game(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
     name: str
     price: Decimal
     launch: str
