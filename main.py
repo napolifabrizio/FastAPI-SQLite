a8bcf37853ec70cb6e2950c89c4dbb6ee1d019d9
Fabrizio <fabrizionapoli50@gmail.com>
2024-01-29 20:34:33 -0300

    method put

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from sqlmodel import SQLModel, create_engine, Session, select
 from models.Game import Game
+from typing import Union
+from decimal import Decimal
 
 app = FastAPI()
 
@@ -25,3 +27,27 @@ def post_product(product: Game):
         session.add(product)
         session.commit()
     return 'The product has been inserted'
+
+@app.put('/update_product/{id_item}')
+def put_product(id_item: int, product: Game):
+    with Session(engine) as session:
+        statement = select(Game).where(Game.id == id_item)
+        result = session.exec(statement)
+        game = result.one()
+        if  product.name != None:
+            game.name = product.name
+            session.add(game)
+            session.commit()
+            session.refresh(game)
+        if product.price != None:
+            game.price = product.price
+            session.add(game)
+            session.commit()
+            session.refresh(game)
+        if product.launch != None:
+            game.launch = product.launch
+            session.add(game)
+            session.commit()
+            session.refresh(game)
+        
+    return 'Product has been updated'
--- models/Game.py
+++ models/Game.py
@@ -1,9 +1,9 @@
 from decimal import Decimal
 from sqlmodel import SQLModel, Field
-from typing import Optional
+from typing import Optional, Union
 
 class Game(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
-    name: str
-    price: Decimal
-    launch: str
+    name: Union[str, None]
+    price: Union[Decimal, None]
+    launch: Union[str, None]
