f3db81a43006645a218f7b9f3f3aa2d448a26277
Fabrizio <fabrizionapoli50@gmail.com>
2024-02-03 01:07:40 -0300

    segunda etapa de config concluida

--- source/config/produto_repo.py
+++ source/config/produto_repo.py
@@ -26,4 +26,33 @@ class ProdutoConfig():
         with Session(engine) as session:
             session.add(product)
             session.commit()
+        return True
+    
+    @staticmethod
+    def put_product(id_item, product, tabela):
+        with Session(engine) as session:
+            statement = select(tabela).where(tabela.id == id_item)
+            result = session.exec(statement)
+            game = result.one()
+
+            if  product.name != None:
+                game.name = product.name
+            if product.price != None:
+                game.price = product.price
+            if product.launch != None:
+                game.launch = product.launch
+            session.add(game)
+            session.commit()
+            session.refresh(game)
+        
+        return True
+    
+    @staticmethod
+    def delete_product(id_item, tabela):
+        with Session(engine) as session:
+            statement = select(tabela).where(tabela.id == id_item)
+            result = session.exec(statement)
+            game = result.one()
+            session.delete(game)
+            session.commit()
         
--- source/main.py
+++ source/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
 from source.config.produto_repo import ProdutoConfig
 from source.models.Game import Game
 
@@ -23,40 +23,14 @@ def post_product(product: Game):
     ProdutoConfig.post_product(product)
     return 'The product has been inserted'
 
-# @app.put('/update_product/{id_item}')
-# def put_product(id_item: int, product: Game):
-#     with Session(engine) as session:
-#         try:
-#             statement = select(Game).where(Game.id == id_item)
-#             result = session.exec(statement)
-#             game = result.one()
-#         except:
-#             raise HTTPException(404, 'Product not found')
-        
-#         if  product.name != None:
-#             game.name = product.name
-#         if product.price != None:
-#             game.price = product.price
-#         if product.launch != None:
-#             game.launch = product.launch
-#         session.add(game)
-#         session.commit()
-#         session.refresh(game)
-        
-#     return 'The product has been updated'
+@app.put('/update_product/{id_item}')
+def put_product(id_item: int, product: Game):
+    ProdutoConfig.put_product(id_item, product, Game) 
+    return 'The product has been updated'
 
-# @app.delete('/delete_product/{id_item}')
-# def delete_product(id_item: int):
-#     with Session(engine) as session:
-#         try:
-#             statement = select(Game).where(Game.id == id_item)
-#             result = session.exec(statement)
-#             game = result.one()
-#         except:
-#             raise HTTPException(404, 'Product not found')
-#         session.delete(game)
-#         session.commit()
-
-#     return 'The product has been deleted'
+@app.delete('/delete_product/{id_item}')
+def delete_product(id_item: int):
+    ProdutoConfig.delete_product(id_item, Game)
+    return 'The product has been deleted'
 
         
